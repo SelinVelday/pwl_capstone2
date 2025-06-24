e84a7a450ccec39e40a6848d07a48869125a8362
SelinVelday <2272048@maranatha.ac.id>
2025-06-25 05:52:27 +0700

    commit lagee

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\DB; // <-- PERUBAHAN: Ditambahkan
-use Carbon\Carbon;                   // <-- PERUBAHAN: Ditambahkan
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;                   
 
 class AdminController extends Controller
 {
